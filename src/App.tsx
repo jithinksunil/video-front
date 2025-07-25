5cd75d03d01d5e7b6a133dbae19c0e0d3a8779dc
jithinksunil <jithinksunil96@gmail.com>
2025-07-25 17:59:29 +0530

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -177,6 +177,7 @@ const SynchronizedVideoPlayer: React.FC = () => {
             ref={containerRef}
           >
             <video
+              playsInline
               ref={videoRef}
               src={videoFile || url}
               className='shadow-lg w-full'
